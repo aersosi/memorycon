17a758272b398c934081acae541b868e0086c0f4
aersosi <acerelroy@gmail.com>
2025-05-28 22:31:15 +0200

    feat(testing): setup jest and implement initial component test - Add Jest configuration and setup files - Configure Babel for testing environment - Add testing-library dependencies - Create mock for game context - Implement first test for StartHeader component - Add test scripts to package.json

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+        '^.+\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '^@/contexts/gameContext': '<rootDir>/src/__mocks__/gameContext.tsx',
+    },
+    transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    },
+};
